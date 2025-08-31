3f3b375eb6a00fcbc7f074a234d0315ea1a2ce3f
iranhonorato <antonioihsj@al.insper.edu.br>
2025-08-31 05:48:53 -0300

    Adicionando imagens e textos à page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,40 @@ import Footer from "../components/Footer";
 export default function Home() {
   return (
     <div className={classes.page}>
-      <div className={classes.geral}>
-        <div className={classes.text_area}>
-          <p>Área destinada para texto</p>
-        </div>
-        <div className={classes.vid_area}>
-          <p>Área destinada para vídeo</p>
+
+
+      <div className={classes.container}>
+        <div className={classes.geral}>
+          <div className={classes.text_area}>
+            <h1>Título</h1>
+            <p>Descrição</p>
+          </div>
+          <div className={classes.vid_area}>
+            <h1>Vídeo</h1>
+          </div>
         </div>
       </div>
+
+
+
       <div className={classes.complete_features}>
-        <div className={classes.description_features}>
-          <p>Descricaoooooooooooooooooooooooooo</p>
+        <div className={classes.study_description}>
+          <h1>Evidências Científicas</h1>
+          <p>
+            O estudo <a href="https://aaafoundation.org/prevalence-drowsy-driving-crashes-estimates-large-scale-naturalistic-driving-study/?utm_source=chatgpt.com">
+            “Prevalence of Drowsy‑Driving Crashes: Estimates from a Large‑Scale Naturalistic Driving Study”, 
+            conduzido pela AAA Foundation for Traffic Safety com base no SHRP 2 NDS</a>, teve como propósito
+            estimar a verdadeira proporção de acidentes de trânsito envolvidos por sonolência do condutor, 
+            identificando casos que frequentemente não são reportados nas estatísticas oficiais. 
+            O estudo demonstrou que a sonolência está envolvida significativamente mais em acidentes do que sugere os relatórios oficiais.
+            Quando observada de forma objetiva, 8,8–9,5% de todos os acidentes e 10,6–10,8% dos acidentes reportáveis à polícia envolvem sonolência do condutor.
+            O método naturalístico com vídeo fornece uma visão mais realista do estado do condutor antes do acidente, enquanto os dados tradicionais (baseados em depoimentos ou relatórios policiais) subestimam esse risco
+          </p>
         </div>
-        <div className={classes.features}>
-          <div>a0</div>
-          <div>a1</div>
-          <div>a2</div>
-          <div>a3</div>
-          <div>a4</div>
-          <div>a5</div>
-          <div>a6</div>
-          <div>a7</div>
-          <div>a8</div>
-          <div>a9</div>
+        <div className={classes.study_data}>
+          <img src="/sono-faixa-etaria.png"/>
+          <img src="/sono-genero.png"/>
+          <img src="/sono-periodo.png"/>
         </div>
       </div>  
     </div>
